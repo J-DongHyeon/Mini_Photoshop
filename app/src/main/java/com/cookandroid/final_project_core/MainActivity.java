67194b9389c54047d3c6dba75a182ab0a1a91d88
DESKTOP-MG12BQR\jdh38 <jdh3876@naver.com>
2021-11-19 21:31:08 +0900

    add - draw line

--- app/src/main/java/com/cookandroid/final_project_core/MainActivity.java
+++ app/src/main/java/com/cookandroid/final_project_core/MainActivity.java
@@ -10,6 +10,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.BlurMaskFilter;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.ColorMatrix;
 import android.graphics.ColorMatrixColorFilter;
 import android.graphics.EmbossMaskFilter;
@@ -18,9 +19,11 @@ import android.graphics.Path;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.AttributeSet;
 import android.view.ContextMenu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -31,6 +34,7 @@ import android.widget.Toast;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.security.KeyStore;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -53,6 +57,7 @@ public class MainActivity extends AppCompatActivity {
     final int NORMAL = 1, INNER = 2, OUTER = 3, SOLID = 4;
     int sel_blur_type = NORMAL;
     boolean control_contrast = false;
+    boolean control_pen = false;
 
     float scaleX = 1, scaleY = 1;
     float angle = 0;
@@ -60,6 +65,8 @@ public class MainActivity extends AppCompatActivity {
     float blur_radius = 50.0f;
     int brightness = 0;
     int bright_sign = 1;
+    int pen_thickness = 3;
+    int pen_color = Color.BLACK;
 
 
     @Override
@@ -91,6 +98,7 @@ public class MainActivity extends AppCompatActivity {
 
         registerForContextMenu(rotate);
         registerForContextMenu(blur);
+        registerForContextMenu(pen);
 
 
 
@@ -245,6 +253,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        pen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                control_pen = !control_pen;
+
+            }
+        });
+
 
 
 
@@ -291,6 +307,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreateContextMenu(menu, v, menuInfo);
 
         MenuInflater menuInflater = getMenuInflater();
+
         if (v == rotate) {
             menu.setHeaderTitle("회전 방향");
 
@@ -299,6 +316,10 @@ public class MainActivity extends AppCompatActivity {
             menu.setHeaderTitle("블러링 종류");
 
             menuInflater.inflate(R.menu.menu_blur, menu);
+        } else if (v == pen) {
+            menu.setHeaderTitle("선 두께");
+
+            menuInflater.inflate(R.menu.menu_pen, menu);
         }
 
     }
@@ -329,6 +350,47 @@ public class MainActivity extends AppCompatActivity {
                 sel_blur_type = SOLID;
                 myview.invalidate();
                 break;
+            case R.id.mm3 :
+                pen_thickness = 3;
+                myview.invalidate();
+                break;
+            case R.id.mm5 :
+                pen_thickness = 5;
+                myview.invalidate();
+                break;
+            case R.id.mm10 :
+                pen_thickness = 10;
+                myview.invalidate();
+                break;
+            case R.id.mm15 :
+                pen_thickness = 15;
+                myview.invalidate();
+                break;
+            case R.id.mm20 :
+                pen_thickness = 20;
+                myview.invalidate();
+                break;
+            case R.id.black :
+                pen_color = Color.BLACK;
+                myview.invalidate();
+                break;
+            case R.id.gray :
+                pen_color = Color.GRAY;
+                myview.invalidate();
+                break;
+            case R.id.red :
+                pen_color = Color.RED;
+                myview.invalidate();
+                break;
+            case R.id.green :
+                pen_color = Color.GREEN;
+                myview.invalidate();
+                break;
+            case R.id.blue :
+                pen_color = Color.BLUE;
+                myview.invalidate();
+                break;
+
         }
 
         return false;
@@ -357,9 +419,22 @@ public class MainActivity extends AppCompatActivity {
 
 
     private class myView extends View {
-        public myView(Context context) { super(context); }
 
-        private Path path = new Path();
+        Paint paint[] = new Paint[100];
+        Path path[] = new Path[100];
+        int path_idx = 1;
+
+        public myView(Context context) {
+            super(context);
+            init();
+        }
+
+        private void init() {
+            for(int i=0; i<path.length; i++) {
+                paint[i] = new Paint();
+                path[i] = new Path();
+            }
+        }
 
         @Override
         protected void onDraw(Canvas canvas) {
@@ -371,7 +446,10 @@ public class MainActivity extends AppCompatActivity {
             canvas.scale(scaleX, scaleY, cenX, cenY);
             canvas.rotate(angle, cenX, cenY);
 
-            Paint paint = new Paint();
+            paint[path_idx].setColor(pen_color);
+            paint[path_idx].setStyle(Paint.Style.STROKE);
+            paint[path_idx].setStrokeWidth(pen_thickness);
+
 
             if (control_contrast) {
                 bright_sign = -1;
@@ -389,7 +467,7 @@ public class MainActivity extends AppCompatActivity {
             ColorMatrix cm = new ColorMatrix(array);
             if (control_gray) cm.setSaturation(0);
 
-            paint.setColorFilter(new ColorMatrixColorFilter(cm));
+            paint[0].setColorFilter(new ColorMatrixColorFilter(cm));
 
             BlurMaskFilter bMask;
 
@@ -408,20 +486,57 @@ public class MainActivity extends AppCompatActivity {
             }
 
             if (control_blur) {
-                paint.setMaskFilter(bMask);
+                paint[0].setMaskFilter(bMask);
             } else {
-                paint.setMaskFilter(null);
+                paint[0].setMaskFilter(null);
             }
-
 
 
             if (control_getImg) {
                 int picX = (view_layout.getWidth() - getImg_buffer.getWidth()) / 2;
                 int picY = ((view_layout.getHeight() - getImg_buffer.getHeight()) / 2);
-                canvas.drawBitmap(getImg_buffer, picX, picY, paint);
+                canvas.drawBitmap(getImg_buffer, picX, picY, paint[0]);
             }
 
+
+
+            for (int i=1; i<=path_idx; i++) {
+                canvas.drawPath(path[i], paint[i]);
+            }
+
+
         }
+
+
+
+        @Override
+        public boolean onTouchEvent(MotionEvent event) {
+            int x = (int) event.getX();
+            int y = (int) event.getY();
+
+            if (control_pen) {
+
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        path[path_idx].moveTo(x, y);
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        path[path_idx].lineTo(x, y);
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        path[path_idx++].lineTo(x, y);
+                        if (path_idx > 100) path_idx = 100;
+                        break;
+
+                }
+
+            }
+
+            invalidate();
+            return true;
+        }
+
+
 
     }
 
